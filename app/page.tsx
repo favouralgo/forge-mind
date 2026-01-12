7b896f37cb147f6476d1689c620497f5cdfafebc
Favour M <favourmadubuko1@gmail.com>
2026-01-12 22:36:04 +0000

    update file

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
       <main className="">
         <h1>ForgeMind AI Productivity App (Forge Sharper Thinking)</h1>
+        <Button>Add Mind</Button>
         
       </main>
 
