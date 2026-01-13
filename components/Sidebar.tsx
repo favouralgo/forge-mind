de9610fb97f46fc0d7c81301a197ecb1c010eaff
Favour M <favourmadubuko1@gmail.com>
2026-01-13 14:59:59 +0000

    Add custom sidebar component

---
+++ components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import NewDocumentButton from "./NewDocumentButton";
+
+function Sidebar() {
+  return (
+    <div className="p-2 md:p-5 bg-gray-200 relative">
+      <NewDocumentButton />
+    </div>
+  );
+} 
+
+export default Sidebar;
