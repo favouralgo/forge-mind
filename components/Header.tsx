211dc9ff3c296c87ae3cd0fc93e073711b64bc36
Favour M <favourmadubuko1@gmail.com>
2026-01-19 14:44:16 +0000

    fix(ui): modify navbar to relative position

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,32 @@
 'use client'
-export default function Header() {
+
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { useUser } from "@clerk/nextjs"
+import { use } from "react"
+
+function Header() {
+    const { user } = useUser();
+
   return (
-    <div>Header</div>
+    <div className="flex items-center justify-between p-5">
+        {user && (
+            <h1 className="text-2xl">
+                {user?.firstName}{`'s`} Mind</h1>
+        )}
+
+        {/* Breadcrumbs or sidebar navigation here */}
+        <div>
+            <SignedOut>
+                <SignInButton/>
+            </SignedOut>
+
+            <SignedIn>
+                <UserButton/>
+            </SignedIn>
+        </div>
+        
+    </div>
   )
 }
+
+export default Header;
