9f05bb17a2f34d239335a3f09e99ea63701a5e67
Favour M <favourmadubuko1@gmail.com>
2026-01-19 14:45:42 +0000

    fix(ui): add hamburger menu to sidebar for responsiveness

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,58 @@
+import { Menu, MenuIcon } from "lucide-react";
 import NewDocumentButton from "./NewDocumentButton";
+// import {
+//   Drawer,
+//   DrawerClose,
+//   DrawerContent,
+//   DrawerDescription,
+//   DrawerFooter,
+//   DrawerHeader,
+//   DrawerTitle,
+//   DrawerTrigger,
+// } from "@/components/ui/drawer"
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 
 function Sidebar() {
+    const menuOptions = (   
+        <> 
+            <NewDocumentButton />
+
+            {/* My Minds */}
+            {/* List... */}
+
+            {/* Shared Minds */}
+            {/* List... */}
+        </>
+    );
   return (
     <div className="p-2 md:p-5 bg-gray-200 relative">
-      <NewDocumentButton />
+        <div className="md:hidden">
+            <Sheet>
+                <SheetTrigger>
+                    <MenuIcon className="p-2 hover:opacity-30 rounded-lg" size={40}/>
+                </SheetTrigger>
+                <SheetContent side="left">
+                  <SheetHeader>
+                    <SheetTitle>Menu</SheetTitle>
+                    <div>
+                        {menuOptions}
+                    </div>
+                  </SheetHeader>
+                </SheetContent>
+            </Sheet>
+        </div>
+        <div className="hidden md:inline">
+            {menuOptions}
+        </div>
     </div>
   );
-} 
-
+}
 export default Sidebar;
